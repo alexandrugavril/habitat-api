97f254bee5bb4e8753e3a93546b1d9a13698d229
Alexandru Gavril <alexandru.gavril.florin@gmail.com>
2019-11-19 13:53:09 +0200

    Added sonar params

--- habitat_baselines/config/default.py
+++ habitat_baselines/config/default.py
@@ -94,16 +94,17 @@ _C.DETECTOR.out_size = 32
 # -----------------------------------------------------------------------------
 _C.PEPPER = CN()
 _C.PEPPER.BufferSize = 10
-_C.PEPPER.ForwardStep = 0.1
-_C.PEPPER.TurnStep = 0.15
+_C.PEPPER.ForwardStep = 0.25
+_C.PEPPER.TurnStep = 0.08725
 _C.PEPPER.RGBTopic = "/pepper_robot/naoqi_driver/camera/front/image_raw"
 _C.PEPPER.DepthTopic = "/pepper_robot/naoqi_driver/camera/depth/image_raw"
 _C.PEPPER.MoveTopic = "/move_base_simple/goal"
 _C.PEPPER.PoseTopic = "/slam_out_pose"
 _C.PEPPER.GoalTopic = "/clicked_point"
-_C.PEPPER.DisplayImages = False
+_C.PEPPER.SonarTopic = "/pepper_robot/naoqi_driver/sonar/front"
 
-_C.PEPPER.EpisodePath = "./pepper_save.p"
+_C.PEPPER.DisplayImages = False
+_C.PEPPER.EpisodePath = "19.11.2019 11:46:14pepper_save.p"
 
 # -----------------------------------------------------------------------------
 # PROXIMAL POLICY OPTIMIZATION (PPO)
